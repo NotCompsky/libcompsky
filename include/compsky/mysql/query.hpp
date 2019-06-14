691f19e0650d062b950d6b0cc45fc3429e29f55d
NotCompsky <adammarkgray@gmail.com>
2019-06-14 10:26:21 +0100

    Renamed assign_next_result to assign_next_row for clarity, and to open a space for potentially adding assign_next_result (not planned, but a possibility).

--- include/compsky/mysql/query.hpp
+++ include/compsky/mysql/query.hpp
@@ -71,7 +71,7 @@ template<typename... Args>
 void assign_next_column(MYSQL_ROW row,  int* col,  asciify::flag::StrLen f,  size_t*& d,  Args... args);
 
 template<typename... Args>
-bool assign_next_result(MYSQL_RES* res,  MYSQL_ROW* row,  Args... args);
+bool assign_next_row(MYSQL_RES* res,  MYSQL_ROW* row,  Args... args);
 
 } // END namespace compsky::mysql
 
@@ -299,7 +299,7 @@ void assign_next_column(MYSQL_ROW row,  int* col,  asciify::flag::StrLen f,  siz
 
 
 template<typename... Args>
-bool assign_next_result(MYSQL_RES* res,  MYSQL_ROW* row,  Args... args){
+bool assign_next_row(MYSQL_RES* res,  MYSQL_ROW* row,  Args... args){
     if ((*row = mysql_fetch_row(res))){
         int col = 0;
         assign_next_column(*row, &col, args...);
