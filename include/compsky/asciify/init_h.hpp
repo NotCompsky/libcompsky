675ef61a9246cad1e07be67796ec2860287235c6
NotCompsky <adammarkgray@gmail.com>
2019-07-27 20:00:07 +0100

    Feature: 'alloc' function to simplify BUF malloc.

---
+++ include/compsky/asciify/init_h.hpp
@@ -0,0 +1,16 @@
+#ifndef LIBCOMPSKY_ASCIIFY_INIT_h_HPP
+#define LIBCOMPSKY_ASCIIFY_INIT_H_HPP
+
+
+namespace compsky {
+namespace asciify {
+
+
+inline
+bool alloc(const size_t sz);
+
+
+} // end namespace asciify
+} // end namespace compsky
+
+#endif
