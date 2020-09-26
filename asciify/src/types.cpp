2f7aeb583718d39fbe23efd2f8f602ab0c6f8323
NotCompsky <adammarkgray@gmail.com>
2020-09-26 21:43:28 +0100

    Tidy		Remove CMake code by moving towards being a header-only library.

--- asciify/src/types.cpp
+++
@@ -1,9 +0,0 @@
-#include "compsky/asciify/types.hpp"
-
-namespace compsky {
-    namespace asciify {
-        namespace fake_type {
-            const Infinity infinity;
-        }
-    }
-}
--- include/compsky/security/memset.hpp
+++ include/compsky/security/memset.hpp
@@ -7,7 +7,22 @@
 
 namespace compsky {
 namespace security {
-void memzero_secure(void* data,  size_t len);
+
+
+inline
+void memzero_secure(void* data,  size_t len){
+    // Based on work by Zhaomo Yang, of the University of California, who released it into the public domain.
+  #ifdef _WIN32
+    SecureZeroMemory (data, len);
+  #elif defined(__GNUC__) || defined(__clang__)
+    memset(data, 0, len);
+    __asm__ __volatile__("" : : "r"(data) : "memory");
+  #else
+    volatile char *p = (volatile char *) data;
+    while (len--)
+      *p++ = 0;
+  #endif
+}
 
 template<typename T>
 int count_digits(T m){
